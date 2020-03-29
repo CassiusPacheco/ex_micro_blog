ed6bfc1523a8db479722a7a96bcffefadd9911ad
Cassius Pacheco <cassius.op@icloud.com>
2020-03-30 00:50:19 +1100

    Setup use live view

--- lib/ex_micro_blog_web/controllers/user_controller.ex
+++ lib/ex_micro_blog_web/controllers/user_controller.ex
@@ -1,19 +1,32 @@
 defmodule ExMicroBlogWeb.UserController do
   use ExMicroBlogWeb, :controller
 
+  import Phoenix.LiveView.Controller
+
   alias ExMicroBlog.Accounts
-  alias ExMicroBlog.Timeline
 
   def index(conn, %{"handler" => handler}) do
-    with user when not is_nil(user) <- Accounts.get_user_by_handler(handler),
-         posts <- Timeline.get_user_posts_by_user_id(user.id) do
-      render(conn, "index.html", user: user, posts: posts)
-    else
+    case Accounts.get_user_by_handler(handler) do
+      %Accounts.User{} = user ->
+        live_render(conn, ExMicroBlogWeb.UserLive, session: %{"user_id" => user.id})
+
       _ ->
         conn
         |> put_status(:not_found)
         |> put_view(ExMicroBlogWeb.ErrorView)
         |> render(:"404")
     end
+
+    # with user when not is_nil(user) <- Accounts.get_user_by_handler(handler),
+    #      posts <- Timeline.get_user_posts_by_user_id(user.id) do
+    #   live_render(conn, ExMicroBlogWeb.UserLive, session: %{"user_id" => user.id}
+    #   # render(conn, "index.html", user: user, posts: posts)
+    # else
+    #   _ ->
+    #     conn
+    #     |> put_status(:not_found)
+    #     |> put_view(ExMicroBlogWeb.ErrorView)
+    #     |> render(:"404")
+    # end
   end
 end
---
+++ lib/ex_micro_blog_web/live/user_live.ex
@@ -0,0 +1,9 @@
+defmodule ExMicroBlogWeb.UserLive do
+  use Phoenix.LiveView
+
+  def mount(_params, %{"user_id" => user_id}, socket) do
+    posts = ExMicroBlog.Timeline.get_user_posts_by_user_id(user_id)
+    user = ExMicroBlog.Accounts.get_user!(user_id)
+    {:ok, assign(socket, user: user, posts: posts)}
+  end
+end
